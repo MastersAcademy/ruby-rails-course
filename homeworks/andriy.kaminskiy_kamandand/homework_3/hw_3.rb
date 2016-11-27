6693f1841c37ca3f931e07d2c6df7a8f5dbd2b44
kamandand <kamandand1@gmail.com>
2016-11-27 22:37:57 +0200

    HW_3 fixed

--- homeworks/andriy.kaminskiy_kamandand/homework_3/hw_3.rb
+++ homeworks/andriy.kaminskiy_kamandand/homework_3/hw_3.rb
@@ -13,20 +13,17 @@ class Patient
   def initialize(age)
     @age=age
   end
-  def get_age
-    return @age
-  end
   def d_therapist
     puts "\nHi! I'm your therapist. What are the symptoms?"
     p_case=Case.new(gets.to_s)
-    p_case.get_case
+    p_case.case_history
     puts "Ok"
     puts p_case.inspect
   end
   def d_pediatrician
     puts "\nHi! I'm your pediatrician. Don't worry, I will heal you. What are the symptoms?"
     p_case=Case.new(gets.to_s)
-    p_case.get_case
+    p_case.case_history
     puts "Ok"
     puts p_case.inspect
   end
@@ -36,33 +33,27 @@ class Doctor < Patient
   def initialize(prescription)
     @precepition=prescription
   end
-  def get_prescription
-    return @prescription
-  end
 end
 class Case < Doctor
-  attr_accessor :a_case
-  def initialize(a_case)
-    @case=a_case
-  end
-  def get_case
-    return @case
+  attr_accessor :case_history
+  def initialize(case_history)
+    @case_history=case_history
   end
 end
 count=Registry.new(0)
 patient=Patient.new(gets.to_i)
-if patient.get_age > 16
-  then adult=Patient.new(patient.get_age)
-  puts patient.inspect
-  puts "You need to consult a therapist"
-  adult.d_therapist
-  else child=Patient.new(patient.get_age)
-  puts "You need to consult a pedistrician"
-  child.d_pediatrician
+if patient.age > 16
+    adult=Patient.new(patient.age)
+    puts patient.inspect
+    puts "You need to consult a therapist"
+    adult.d_therapist
+  else child=Patient.new(patient.age)
+    puts "You need to consult a pedistrician"
+    child.d_pediatrician
 end
 puts "I'll write a prescription"
-prescription=Doctor.new(gets.to_s)
-prescription.get_prescription
-puts prescription.inspect
+prescription_1=Doctor.new(gets.to_s)
+prescription_1.prescription
+puts prescription_1.inspect
 count.sum
 puts count.inspect
