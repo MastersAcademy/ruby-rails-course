695411f2320cd487fb6430bf147d885b83344709
ZaliznyakD <dmitro.zalizniak@gmail.com>
2016-11-27 23:59:46 +0200

    HW #3 not finished yet

---
+++ homeworks/dmitriy.zaliznyak_zaliznyakd/homework_3/hw3.rb
@@ -0,0 +1,32 @@
+puts "Home work 3"
+puts "========================================"
+
+class Student
+  attr_reader :stud_id, :stud_FIO, :stud_birth, :stud_adress
+
+  def initialize
+    stud_id = stud_id
+    stud_FIO = stud_FIO
+    stud_birth = stud_birth
+    stud_adress = stud_adress
+  end
+end
+
+class Teacher
+  attr_reader :teach_id, :teach_FIO, :teach_lesson
+
+  def initialize
+    teach_id = teach_id
+    teach_FIO = teach_FIO
+    teach_lesson = teach_lesson
+  end
+end
+
+class Lesson
+
+end
+
+class Journal
+
+end
+
