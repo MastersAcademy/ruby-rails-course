f41f019bcd67f9c1084b682480db2eaff9d49fb9
squirrel111 <anastasiia.andrieieva@gmail.com>
2016-11-28 23:59:28 +0200

    garden

--- homeworks/anastasiia.andrieieva_squirrel111/homework_3/g.rb
+++ homeworks/anastasiia.andrieieva_squirrel111/homework_3/g.rb
@@ -2,12 +2,13 @@
 
 class Garden
 attr_reader :length, :width
+  
   def initialize(length, width)
     @length, @width = length, width
   end
 
   def hello
-    puts "Привет, я вскопаный огород, ждущий посадки.Мои параметры: "
+    puts "It is garden!"
   end
 end
 
@@ -18,30 +19,27 @@ gargen = Garden.new("10m","10m")
 
 class Seedling
 attr_accessor :colour, :stem_lenght, :root_lenght
+  
   def initialize(colour = "green", stem_lenght = "10cm", root_lenght = "5cm")
     @colour, @stem_lenght, @root_lenght = colour, stem_lenght, root_lenght
   end  
 
   def hello
-    puts "Я - рассада! Посади меня!"
-  end
-
-  def Seedling.factory(sg)
-    { :Paprica => Paprica, :Tomato => Tomato }[sg].new
+    puts "it is seedling. Grow it!"
   end
 end
 
-seedling = Seedling.new("цвет = зелёный", "стебель = 10-15cm", "корень = 5-10см")
+seedling = Seedling.new(colour = "green", stem_lenght = "10cm", root_lenght = "5cm")
   puts seedling.colour
   puts seedling.stem_lenght
   puts seedling.root_lenght
   puts seedling.hello
-
+ 
 
 class Paprica < Seedling
 
   def identification 
-    puts "Я выросту и буду сладким перчиком!"
+    puts "It is paprica!"
   end
 end
 
@@ -53,11 +51,125 @@ puts paprica.identification
 class Tomato < Seedling
 
   def identification 
-    puts "Я выросту и буду сочным томатом!"
+    puts "It is tomato!"
   end
 end
 
 tomato = Tomato.new
 puts tomato.hello
 puts tomato.identification
+
+class SeedlingFactory
+  def self.build(seedling)
+    case seedling
+    when :tomato
+     Tomato.new
+    when :paprica
+     Paprica.new
+    end
+  end
+end
+
+class Instrument
+  attr_accessor :name
+  def initialize(name)
+  @name = name
+  end
+  
+  def hello
+    puts "it is #{@name}."
+  end
+end
+
+shovel = Instrument.new('shovel')
+bucket = Instrument.new("bucket")
+water_pail = Instrument.new("water_pail")
+water_hose = Instrument.new("water_hose")
+
+puts shovel.hello
+puts bucket.hello
+puts water_pail.hello
+puts water_hose.hello
+
+
+
+class Gardeners
+  def initialize (first_name, sex, age)
+    @name, @sex, @age = first_name, sex, age
+  end
+  
+  def working
+    puts "pick up a shovel"
+    puts "get out of the hands of a shovel"
+    puts "shovel to dig wells 15 to 15 cm, 20 cm deep, with an interval of 30 cm"
+    puts "22.times do"
+    puts "go to the next row"
+    puts "make watering for all rows"
+    puts "after watering take shovel"
+    puts "plant "
+    end
+end
+
+class Mother  < Gardeners
+  
+  def hello
+    puts "She is Alice"
+  end
+
+  def watering
+    puts "take a bucket"
+    puts "to put a bucket under water_pail"
+    puts "open water_pail"
+    puts "pour into a bucket of water"
+    puts "close water_pail"
+    puts "pour a bucket of rows"
+    end
+
+  def plant
+    seedling = SeedlingFactory.build(:tomato)
+    seedling.identification
+    puts "take a  tomato"
+    puts "put  tomato in a hole"
+    puts "100.times do"
+    puts "fill all holes earth"
+  end
+end
+
+mother = Mother.new("Alice", "female", "35 years")
+puts mother.hello
+puts mother.working
+puts mother.watering
+puts mother.plant
+
+class Son < Gardeners
+  def hello
+    puts "He is Adam"
+  end
+
+  def waterning
+    puts "take a water_hose"
+    puts "to connect a water_hose with water_pail"
+    puts "open water_pail"
+    puts "pit pour water from a water_hose"
+    puts "close water_pail"
+  end
+
+     def plant
+    seedling = SeedlingFactory.build(:paprica)
+    seedling.identification
+    puts "take a  paprica"
+    puts "put  paprica in a hole"
+    puts "100.times do"
+    puts "fill all holes earth"
+  end
+end
+
+
+son = Son.new("Adam", "male", "14 years")
+puts son.hello
+puts son.working
+puts son.waterning
+puts son.plant
+
+
 
