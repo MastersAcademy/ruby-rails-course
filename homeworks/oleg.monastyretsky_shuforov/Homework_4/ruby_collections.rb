84110aa7b21542fddd7174d7b254d1337f77bd8e
shuforov <shuforov@gmail.com>
2016-12-02 12:54:37 +0200

    change creation of hash from Hash to {}, creation of set going via each_with_object method

--- homeworks/oleg.monastyretsky_shuforov/Homework_4/ruby_collections.rb
+++ homeworks/oleg.monastyretsky_shuforov/Homework_4/ruby_collections.rb
@@ -5,53 +5,51 @@ require 'set'
 # Strings that are now on the guitar (Inch dimension)
 # Six string guitar
 @machine_heads_guitar = [10, 13, 17, 26, 36, 46]
-electric_guitar = Hash[
-  'Extra Super Light', [8, 10, 15, 21, 30, 38],
-  'Extra Super Light Plus', [8.5, 10.5, 15, 22, 32, 39],
-  'Super Light', [9, 11, 16, 24, 32, 42, 54],
-  'Super Light Plus', [9.5, 11.5, 16, 24, 34, 44],
-  'Super Light / Regular', [9, 11, 16, 26, 36, 46],
-  'Regular Light', [10, 13, 17, 26, 36, 46, 56],
-  'Regular Light Plus', [10.5, 13.5, 18, 28, 38, 48],
-  'Medium', [11, 14, 18, 28, 38, 49, 64],
-  'Light Top / Heavy Bottom', [10, 13, 17, 30, 42, 52],
-  'Medium / Heavy', [11, 14, 18, 30, 42, 52],
-  'Heavy', [12, 16, 20, 32, 42, 54],
-  'Extra Heavy', [13, 17, 26, 36, 46, 56],
-]
+electric_guitar = {
+  'Extra Super Light' => [8, 10, 15, 21, 30, 38],
+  'Extra Super Light Plus' => [8.5, 10.5, 15, 22, 32, 39],
+  'Super Light' => [9, 11, 16, 24, 32, 42, 54],
+  'Super Light Plus' => [9.5, 11.5, 16, 24, 34, 44],
+  'Super Light / Regular' => [9, 11, 16, 26, 36, 46],
+  'Regular Light' => [10, 13, 17, 26, 36, 46, 56],
+  'Regular Light Plus' => [10.5, 13.5, 18, 28, 38, 48],
+  'Medium' => [11, 14, 18, 28, 38, 49, 64],
+  'Light Top / Heavy Bottom' => [10, 13, 17, 30, 42, 52],
+  'Medium / Heavy' => [11, 14, 18, 30, 42, 52],
+  'Heavy' => [12, 16, 20, 32, 42, 54],
+  'Extra Heavy' => [13, 17, 26, 36, 46, 56]
+}
 
-acustic_guitar = Hash[
-  'Extra light', [10, 14, 23, 30, 39, 47],
-  'Custom light', [11, 15, 23, 32, 42, 52],
-  'Light', [12, 16, 25, 32, 42, 54],
-  'Light/Medium', [12.5, 16.5, 25.5, 33.5, 43.5, 56],
-  'Medium', [13, 17, 26, 35, 45, 56],
-  'Heavy', [14, 18, 27, 39, 49, 59],
-]
+acustic_guitar = {
+  'Extra light' => [10, 14, 23, 30, 39, 47],
+  'Custom light' => [11, 15, 23, 32, 42, 52],
+  'Light' => [12, 16, 25, 32, 42, 54],
+  'Light/Medium' => [12.5, 16.5, 25.5, 33.5, 43.5, 56],
+  'Medium' => [13, 17, 26, 35, 45, 56],
+  'Heavy' => [14, 18, 27, 39, 49, 59]
+}
 
-bass_guitar = Hash[
-  'Super Light', [40, 60, 75, 95, 12.5],
-  'Custom Light', [40, 60, 80, 100],
-  'Regular Light', [32, 45, 65, 80, 100, 130],
-  'Regular Medium', [32, 45, 65, 85, 105, 135],
-  'Medium', [50, 70, 85, 105, 135],
-  'Heavy', [55, 75, 90, 110]
-]
+bass_guitar = {
+  'Super Light' => [40, 60, 75, 95, 12.5],
+  'Custom Light' => [40, 60, 80, 100],
+  'Regular Light' => [32, 45, 65, 80, 100, 130],
+  'Regular Medium' => [32, 45, 65, 85, 105, 135],
+  'Medium' => [50, 70, 85, 105, 135],
+  'Heavy' => [55, 75, 90, 110]
+}
 
-guitar_strings = [electric_guitar, acustic_guitar, bass_guitar]
+@guitar_strings = [electric_guitar, acustic_guitar, bass_guitar]
 puts 'All available strings'
 puts '==='
-guitar_strings.each { |guitar_type| puts guitar_type.keys, '' }
-@six_string = Set.new []
-@seven_string = Set.new []
-guitar_strings.slice(0).each do |key, value|
+@guitar_strings.each { |guitar_type| puts guitar_type.keys, '' }
+@six_string = @guitar_strings.slice(0).each_value.each_with_object(Set.new []) do |value, set|
   if value.length == 6
-    @six_string.add(key)
+    set << @guitar_strings.slice(0).key(value)
   end
 end
-guitar_strings.slice(0).each do |key, value|
-  if value.length > 6
-    @seven_string.add(key)
+@seven_string = @guitar_strings.slice(0).each_value.each_with_object(Set.new []) do |value, set|
+  if value.length == 7
+    set << @guitar_strings.slice(0).key(value)
   end
 end
 puts '==='
@@ -59,13 +57,13 @@ begin
   @machine_heads_guitar.fetch(6)
   puts 'Choose strings for seven string guitar'
   @seven_string.each { |string| puts string }
-  @new_strings = guitar_strings.slice(0)[@seven_string.first]
+  @new_strings = @guitar_strings.slice(0)[@seven_string.first]
   puts '==='
 rescue IndexError
   puts 'Choose strings for six string guitar'
   puts '==='
   @six_string.each { |string| puts string }
-  @new_strings = guitar_strings.slice(0)[@six_string.first]
+  @new_strings = @guitar_strings.slice(0)[@six_string.first]
   puts '==='
 end
 
