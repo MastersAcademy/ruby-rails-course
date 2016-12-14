95cc074da2eb031a055355b364585c2372f2a5cc
Sergey Ripchanskiy <sergeyripchanskiy@gmail.com>
2016-12-14 02:04:57 +0200

    HW5_correction

--- homeworks/sergey.ripchanskiy_Ripchanskiy/homework 5/HW5.rb
+++ homeworks/sergey.ripchanskiy_Ripchanskiy/homework 5/HW5.rb
@@ -1,35 +1,72 @@
-module Cashier_name
 
-  attr_accessor :manager_name
-
-  def cashier(cashier_name)
-    $manager_name = cashier_name
+module User_data
+  attr_accessor :name, :surname, :city, :department
+  def info (name, surname, city, department)
+    @name, @surname, @city, @department = name, surname, city, department
   end
 end
 
-module Welcome
-  def hello(status, name)
-    puts "Добрый день #{name}."" Меня зовут #{$manager_name}."
-    puts "Вы хотите поучить или отправить посылку?"
-    
-    if status == "send"
-      puts "Я бы хотел отправить посылку"
-    else 
-      puts "Я бы хотел получить посылку"
+module Opening_hours
+  def open
+    time = Time.now.to_a
+    if time[2] <8 || time[2] >18
+       puts "Отделение работает с 8 до 18. Приходите в рабочее время"
+       puts "или отправляйтесь в отделения круглосуточного обслуживания"
+    else
+      puts "Отделение работает"
     end
   end
 end
 
-module Pay
+
+class Manager
+  include User_data
+  @@counter = 1170003128
+
+  def initialize(name, surname, city, department)
+    info(name, surname, city, department)
+
+    puts "Добрый день! Меня завут #{surname} #{name}" 
+    puts "Я менеджер #{department} отделения, в городе #{city}." 
+    puts "Чем могу помочь?"
+  end
+
+  def declar
+    puts "Ваш номер декларации #{@@counter+=1}"
+  end
+  
+  def recipient
+    puts "Вот Ваша посылка. Возьмите и распишитесь "
+    puts "Хорошего дня"
+  end
+
+end
+
+
+class Sender
+  include User_data
+  extend Opening_hours
+  
+  def initialize(name, surname, city, department)
+    info(name, surname, city, department)
+  end
+
+  def send(recipient_name, recipient_surname, recipient_city, recipient_department)
+    puts "Я хочу отправить посылку:"
+    puts "Получатель: #{recipient_name} #{recipient_surname}"
+    puts "Город #{recipient_city}, отделение #{recipient_department}"
+    puts "Мои данные #{name} #{surname} #{city} #{department}"
+  end
+
   def pay(money)
-    puts "С вас 10 грн за отправку"
+    puts "#{name}, с вас 10 грн за отправку"
     if money == 10
       puts "Вы дали #{money} гривен. Оплачено"
     elsif money >= 100
       puts "У вас не будет купюры поменьше. У меня не будет из #{money} грн сдачи."
     elsif money > 10
       a = money - 10
-      puts "Вы дали #{money} грн. Возьмите, пожалуйста, сдачу, #{a} грн."
+      puts "Вы дали #{money} грн. Возьмите, пожалуйста, сдачу #{a} грн."
       puts "Хорошего дня!"
     else i = 10 - money
       puts "Пожалуйста, заплатите #{i} грн"
@@ -37,24 +74,29 @@ module Pay
   end
 end
 
+class Recipient
+  include User_data
+  extend Opening_hours
 
-class Manager
-  extend Cashier_name
+  def initialize(name, surname, city, department)
+    puts "Добрый день. Я хочу получить посылку"
+    puts "Вот данные для получения #{name}, #{surname}, #{city}, #{department}"
+  end
 end
 
-class Sender
-  include Welcome
-  include Pay
-end
 
-class Reciplient
-  include Welcome
-end
+puts "Отправитель пришёл в отделение укрпочты"
+Sender.open
+puts "Отправитель в отделении"
+manager1 = Manager.new('Svetlana', 'Volkova', 'Borispol', '22')
+sender1 = Sender.new('Boris', 'Sokolovskiy', 'Borispol', '22')
+sender1.send('Pavlo', 'Nazarov', 'Kiev', '5')
+sender1.pay(20)
+manager1.declar
+puts "Получатель пришёл в отделение укрпочты"
+Recipient.open
+puts "Получатель в отделении"
+recipient1 = Recipient.new('Pavlo', 'Nazarov', 'Kiev', '5')
+manager1.recipient
 
-Manager.cashier("Vlada")
-sender1 = Sender.new
-sender1.hello("send", "Sergey Ripchanskiy")
-sender1.pay(11)
-reciplient1 = Reciplient.new
-reciplient1.hello("get", "Gopko Taras")
 
