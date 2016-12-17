3c5345f3dab4227f8bf6a8128168b0ec9cb18430
dimkawp <dimkawp@gmail.com>
2016-12-17 20:05:17 +0200

    Homework-5 fix module and class name. Homework-6 add check errors.

--- homeworks/dima.bondar_dimkawp/homework-6/homework-6.rb
+++ homeworks/dima.bondar_dimkawp/homework-6/homework-6.rb
@@ -3,6 +3,9 @@ module FactorySize
   attr_accessor :size
 
   def size_ball(size)
+
+    raise ArgumentError.new("Rep must be positive Integer") unless size.respond_to?(:to_i) && size.to_i>0
+
     case size
       when 1 .. 2
         p 'little size'
@@ -41,7 +44,6 @@ module FactoryTextile
         puts 'default textile'
     end
 
-
     p "TextileBall - #{textile}"
     p '__________________________'
 
@@ -79,6 +81,8 @@ module FactoryWeigh
   attr_accessor :weigh
 
   def weigh_ball(weigh)
+
+    raise ArgumentError.new("Rep must be positive Integer >200 or <500") unless weigh.respond_to?(:to_i) && weigh.to_i>=200 && weigh.to_i<=500
 
     case weigh
       when 200 .. 300
@@ -150,6 +154,8 @@ class SoccerBall
   include TypeCheck
 
   def initialize
+    @weigh = weigh
+    @size = size
     @types = ['Soccer','soccer','Soccer-Ball']
   end
 
@@ -157,7 +163,6 @@ end
 
 class BasketBall
 
-  extend NameAdd
   include FactorySize
   include FactoryColor
   include FactoryTextile
@@ -177,7 +182,6 @@ class ValleyBall
   include FactoryColor
   include FactoryTextile
   include FactoryWeigh
-  include TypeCheck
 
   def initialize
     @types = ['ValleyBall','valleyball','Vally-Ball']
@@ -187,25 +191,57 @@ end
 
 # Builder
 
-SoccerBall.unique_def
 soccer_ball = SoccerBall.new
+begin
+soccer_ball.unique_def
+rescue NoMethodError
+  include Unique
+else
+  p "If no exception, include module"
+ensure
+  p "Unique is include"
+end
+soccer_ball.unique_def
 soccer_ball.type_ball('soccer')
 SoccerBall.ball_name()
-soccer_ball.size_ball('default')
+soccer_ball.size_ball(1)
 soccer_ball.textile_ball('12')
 soccer_ball.color_ball('default')
-soccer_ball.weigh_ball('default')
-p '__________________________'
+soccer_ball.weigh_ball(200)
+p '=========================='
+
+
 basket_ball = BasketBall.new
+begin
+basket_ball.ball_name
+rescue NoMethodError
+  include NameAdd
+else
+  p "If no exception, include module"
+ensure
+  p "Module NameAdd is include"
+
+end
 basket_ball.type_ball('default')
 BasketBall.ball_name('default')
-basket_ball.size_ball('default')
+basket_ball.size_ball(1)
 basket_ball.color_ball('default')
-basket_ball.weigh_ball('default')
-p '__________________________'
+basket_ball.weigh_ball(322)
+p '=========================='
+
 vally_ball = ValleyBall.new
+begin
+  vally_ball.type_ball('valleyball')
+rescue NoMethodError
+  include TypeCheck
+else
+  p "If no exception, include module"
+ensure
+  p "module TypeCheck is include"
+end
 vally_ball.type_ball('Vally-Ball')
 ValleyBall.ball_name('default')
-vally_ball.size_ball('default')
+vally_ball.size_ball(3)
 vally_ball.color_ball('default')
-vally_ball.weigh_ball('default')
+vally_ball.weigh_ball(450)
+p '=========================='
