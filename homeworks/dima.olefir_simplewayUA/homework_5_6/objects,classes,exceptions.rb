253bbb99c26abe38ba11b0a2c0aceddceb634b21
simpleway <itfortim@gmail.com>
2016-12-18 17:36:07 +0200

    homework

---
+++ homeworks/dima.olefir_simplewayUA/homework_5_6/objects,classes,exceptions.rb
@@ -0,0 +1,107 @@
+# Hospital
+puts "Здравствуйте, Ваш номер карты?"
+
+module Describer
+  
+  def describe(description)
+    puts @description = description
+  end
+end
+
+module Inspection
+  def number(value)
+    raise StatementError.new("Error! The '#{value}' must be a number and greater than zero.") unless value.respond_to?(:to_i) && value.to_i > 0
+  end
+end
+
+class StatementError < StandardError
+
+  def initialize(part)
+    puts part
+  end
+end
+
+class Receptionist
+  include Describer
+  include Inspection
+
+  def initialize( id_card = "2913",
+                  last_name = "Петрович",
+                  first_name = "Петр",
+                  location = "Черкасская обл., Черкассы",
+                  medical_history = "история больного(записи)")
+    $id_card = id_card
+    $last_name = last_name
+    $first_name = first_name 
+    $location = location
+    $medical_history = medical_history
+  end
+
+  def say_hi(id_card = 2913)
+    begin
+      number(id_card)
+    rescue StatementError
+      $id_card = 2913
+    else
+      $id_card = id_card
+    end
+    describe "Номер моей карты, - #{$id_card}!"
+  end
+end
+
+class Patient
+  include Describer
+
+  def medical_record
+    describe "Данные по мед. карте № #{$id_card}\n#{$last_name} #{$first_name}\n#{$location}\n#{$medical_history}"  
+  end
+end
+
+class Doctor
+  include Describer
+
+  def initialize(consulting_room = "12",
+                  name_doctor = "Ай-Болит",
+                  recording_disease = "таблетку")
+    $consulting_room = consulting_room
+    $name_doctor = name_doctor
+    $recording_disease = recording_disease
+  end
+
+  def visit
+    describe "Кабинет №#{$consulting_room}, врач #{$name_doctor}\nВрач вызывает пациента #{$last_name} #{$first_name}!"
+  end
+
+  def review
+    describe "Осмотр пациента\nБоль в животе, врач назначил #{$recording_disease}"
+  end
+end
+
+class Check
+  attr_accessor :date, :time, :price
+
+  def initialize(date, time, price)
+    @date = date
+    @time = time
+    @price = price
+  end
+
+  def issue_check
+    puts " Дата: #{@date}"
+    puts "Время: #{@time}"
+    puts " Цена: #{@price}"
+  end
+end
+
+salute = Receptionist.new
+salute.say_hi
+
+hello = Patient.new
+hello.medical_record
+
+doc = Doctor.new
+doc.visit
+doc.review
+
+issue = Check.new('12.11.2016', '19:00', '150 грн.')
+issue.issue_check
