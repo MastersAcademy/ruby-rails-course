033474996831a824162da9e0d5798199b43baf4a
kostiaCk <kostia.ck@gmail.com>
2016-12-19 23:36:13 +0200

    style-guide

--- homeworks/kostia.ponomarenko_kostiaCk/homework_5/homework_5.rb
+++ homeworks/kostia.ponomarenko_kostiaCk/homework_5/homework_5.rb
@@ -1,110 +1,110 @@
 ﻿# homework 5
 # Ruby example crm
-# https://repl.it/Eptu/2
 
-require 'date'
-
-module Instance_description 
+module InstanceDescription
   def info
-    if self.respond_to?(:name) 
-      puts ">> #{name}." 
-    elsif self.respond_to?(:number) 
-      puts ">> #{self.class} ##{number}." 
+    if self.respond_to?(:name)
+      puts ">> #{name}."
+    elsif self.respond_to?(:number)
+      puts ">> #{self.class} ##{number}."
     else
-      puts ">> #{self}" 
+      puts ">> #{self}"
     end
   end
 end
 
-module Class_description 
-
+module ClassDescription
   def all
     ObjectSpace.each_object(self).to_a
   end
 
   def count
-    all.count
+    if self.class_variable_defined?(:@@counter)
+      self.class_variable_get(:@@counter)
+    else
+      all.count
+    end
   end
-
 end
 
 class Human
   attr_accessor :comment
   attr_reader :name
-  
+  include InstanceDescription
+  extend ClassDescription
+
   def initialize(name, comment)
-    @name, @comment = name, comment
+    @name = name
+    @comment = name
   end
-  include Instance_description
-  extend Class_description
 end
 
 class Goods
-  attr_reader :name, :weight, :price
+  attr_accessor :price
+  attr_reader :name, :weight
+  include InstanceDescription
+  extend ClassDescription
+
   def initialize(name, weight, price)
-    @name, @weight, @price = name, weight, price
+    @name = name
+    @weight = weight
+    @price = price
   end
-  include Instance_description
-  extend Class_description
 end
 
 class DocumentSales
-  
-  attr_accessor :salesGood
+  attr_accessor :sales_good
   attr_reader :number, :date, :customer, :manager, :sum
-  
-  
-  def initialize(number, date, customer, manager)
-    @number, @date, @customer, @manager = number, date, customer, manager
-    @salesGood, @sum= [], 0
+  include InstanceDescription
+  extend ClassDescription
+  @@counter = 0
+
+  def initialize(customer, manager)
+    @date = Time.now
+    @customer = customer
+    @manager = manager
+    @sum = 0
+    @number = "%06d" % @@counter += 1
+    @sales_good = []
   end
 
   def add(goods, amount)
-    @salesGood << DocumentSalesGoods.new(self, goods, amount, goods.price) # 
+    @sales_good << DocumentSalesGoods.new(self, goods, amount, goods.price)
     @sum += goods.price * amount
   end
-
-  include Instance_description
-  extend Class_description
 end
 
 class DocumentSalesGoods
-  attr_reader :documentSales, :goods, :amount, :price
-    def initialize(documentSales, goods, amount, price)
-    @documentSales, @goods, @amount, @price = documentSales, goods, amount, price
+  attr_reader :document_sales, :goods, :amount, :price
+  include InstanceDescription
+  extend ClassDescription
+
+  def initialize(document_sales, goods, amount, price)
+    @document_sales = document_sales
+    @goods = goods
+    @amount = amount
+    @price = price
   end
-  include Instance_description
-  extend Class_description
 end
 
-customer1 = Human.new('Покупатель 1', 'покупатель')
-manager1 = Human.new('Менеджер Коля', 'менеджер')
-goods1 = Goods.new('Хлеб', 0.7, 8)
-goods2 = Goods.new('Пиво 05', 0.7, 12)
+def add_document_sales(customer, manager, mas_goods)
+  new_document_sales = DocumentSales.new(customer, manager)
+  mas_goods.each { |el| new_document_sales.add(el[0],el[1]) }
+end
+
+customer = Human.new('Покупатель 1', 'покупатель')
+manager = Human.new('Менеджер Коля', 'менеджер')
+goods1 = Goods.new('Хлеб', 0.7, 10)
+goods2 = Goods.new('Пиво 05', 0.7, 15)
 goods3 = Goods.new('Клубника 1кг', 1, 30)
 
-DocumentSales1 = DocumentSales.new(1, Date.today, customer1, manager1)
-DocumentSales1.add(goods1, 1)
-DocumentSales1.add(goods2, 2)
-DocumentSales1.add(goods3, 3)
+add_document_sales(customer, manager, [[goods1, 1], [goods2, 2], [goods3, 4]])
+add_document_sales(customer, manager, [[goods1, 1]])
+add_document_sales(customer, manager, [[goods2, 15]])
 
-DocumentSales2 = DocumentSales.new(2, Date.today, customer1, manager1)
-DocumentSales2.add(goods1, 1)
-
-DocumentSales3 = DocumentSales.new(3, Date.today, customer1, manager1)
-DocumentSales3.add(goods2, 15)
-
-
-puts "Всего humanодов: #{Human.count}"
+puts "Всего humanоидов: #{Human.count}"
 Human.all.each { | obj| obj.info }
-
 puts "Всего товаров: #{Goods.count}"
 Goods.all.each { | obj| obj.info }
-
 puts "Всего документов: #{DocumentSales.count}"
 DocumentSales.all.each { | obj| obj.info }
-
-puts "Всего продаж: #{DocumentSalesGoods.count}"
-DocumentSalesGoods.all.each { | obj| obj.info }
-
-nil
