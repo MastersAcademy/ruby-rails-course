a496677a13ef2ad9548c4ae5705a483fed80ac2d
ArtemKenko <beastboy@bk.ru>
2016-12-19 15:50:35 +0200

    homework6

--- homeworks/artem.kyrichenko_artemkenko/homework6.rb
+++ homeworks/artem.kyrichenko_artemkenko/homework6.rb
@@ -5,11 +5,13 @@ class TimeError < StandardError
 end
 
 module PersonInfo
-  attr_accessor :email, :name, :surname
-  def input_info(email, name, surname)
+  @@counter = 0
+  attr_accessor :email, :name, :surname, :id
+  def input_info(email, name, surname, id)
     @name = name
     @surname = surname
     @email = email
+    @id = @@counter+=1
   end
 end
 
@@ -17,21 +19,19 @@ end
 class User
   include PersonInfo
 
-  @@counter = 0
-  attr_accessor :id , :adress, :mass
+  attr_accessor :adress, :mass
 
   def initialize(email, name, surname, adress, mass)
-    input_info(email, name, surname)
+    input_info(email, name, surname, id)
     @adress = adress
-    @id = @@counter+=1
     @mass = mass
     puts "User nuber #{id} - #{name} come to gym"
-    User.rest()
+    User.rest
     rescue NoMethodError => e
-        p e.message
-        p e.backtrace
-        User.class.send(:define_method, :rest) { puts "eat and sleap" }
-        User.rest()
+      p e.message
+      p e.backtrace
+      User.class.send(:define_method, :rest) { puts "eat and sleap" }
+      User.rest
 
   end
 
@@ -48,14 +48,12 @@ end
 
 class Trainer
   include PersonInfo
-  @@counter = 0
-  attr_accessor :trainerid , :experience, :price
+  attr_accessor :experience, :price
 
   def initialize(email, name, surname, experience)
-    input_info(email, name, surname)
+    input_info(email, name, surname, id)
     @experience = experience
-    @trainerid = @@counter+=1
-    puts "Trainer #{trainerid} #{name} #{surname} ready to work"
+    puts "Trainer #{id} #{name} #{surname} ready to work"
   end
 
   def technique(price, cost)
@@ -75,21 +73,21 @@ class Manager
 
   def times_up(time)
     begin
-      raise TimeError.new unless time.to_i>0 && time.to_i<23
+      raise TimeError.new unless time.to_i.between?(0, 23)#time.to_i>0 && time.to_i<23
       rescue TimeError => e
         p e.message
         p e.backtrace
       else
-        if time.to_i >= 8 && time.to_i <= 22
+        if time.to_i.between?(8, 22)
           puts "Gym is open! Come inside, if you want!"
-      else
-        puts "Gym is closed, go away!"
-      end
+        else
+          puts "Gym is closed, go away!"
+        end
     end
   end
 
   def initialize(email, name, surname)
-    input_info(email, name, surname)
+    input_info(email, name, surname, id)
     puts "My name is #{name}, wellcome in our gym"
   end
 end
@@ -100,6 +98,6 @@ trainer1 = Trainer.new('fooboo@mail.ru', 'Bilbo', 'Begins', 15)
 trainer1.technique(50, 60)
 manager1 = Manager.new('beast@mail.ua', 'Sasha', 'Grey')
 puts "==When there an error=="
-manager1.times_up(23)
+manager1.times_up(25)
 puts "==When there isn't=="
 manager1.times_up(9)
