954356dd33c6d4e562819007b0b5ed0e19213a85
serjpetrenko <b.g.lupus@gmail.com>
2016-12-28 14:56:00 +0200

    Added Hometask 5,6  fix

--- homeworks/serhiy.petrenko_serjpetrenko/Homework_6/Hometask_6.rb
+++ homeworks/serhiy.petrenko_serjpetrenko/Homework_6/Hometask_6.rb
@@ -10,49 +10,78 @@ module Welcome
 
   def greeting(type)
     puts "================================="
-    puts "Welcome to the #{type} cocktails!"
+    puts "Welcome to the #{type} cocktails bar!"
     puts "================================="
   end
 end
 
-module ClassicCocktails
-  def ClassicCocktails.cc_set
-    cocktails = {
-      1 => {name: 'Mojito', recipe: "White rum - 50 ml,\nlime - 2 slices,\nmint - 15 gr.,\nsimple syrup - 20 gr."},
-      2 => {name: 'Cuba libre', recipe: "Black rum - 50 ml\nCola - 150 ml\nFresh lime juice - 10 ml"},
-      3 => {name: 'Black russian', recipe: "Vodka - 50 ml\nKahlua - 50 ml"},
-      4 => {name: 'God father', recipe: "Scotch - 35 ml\nAmaretto - 35 ml"},
-    }
+module Guest
+  attr_accessor :name, :drinks
+
+  def initialize(name, drinks)
+    @name = name
+    @drinks = drinks
   end
 end
 
-module NewEra
-  def NewEra.ne_set
-    cocktails = {
-      1 => {name: 'Bramble', recipe: "Gin - 40 ml,\nSugar syrup - 10 ml,\nFresh lemon juice - 15 ml.,\nBlackberry liqueur - 15 ml."},
-      2 => {name: 'French martini', recipe: "Vodka - 45 ml\nRaspberry liqueur - 15 ml.\nFresh pineapple juice - 15 ml"},
-      3 => {name: 'Spritz veneziano', recipe: "Prosseco - 60 ml\nAperol - 40 ml\nSplash of Soda water."},
-      4 => {name: 'Vesper', recipe: "Gin - 60 ml\nVodka - 15 ml\nLillet Blonde - 7 ml."},
-    }
-  end
-end
-
-class InShakerClassic
+class ClassicCocktailsBar
   include Welcome
-  extend ClassicCocktails
+  include Guest
 
   def run1
+
     begin
+      puts "Hello i'm #{@name}, and i need some #{drinks}, what you can offer?"
       puts "Here is a coctails, you can choose one and we show ingredients:"
-      puts "Mojito - 1\nCuba libre - 2\nBlack russian - 3\nGod father - 4\n"
-      puts "Make your  choice: "
+      puts "Mojito - 1\nCuba libre - 2\nBlack russian - 3\nGod father - 4\nMake your  choice: "
       choice = gets.to_i
+      cocktails = {
+          1 => {name: 'Mojito', recipe: "White rum - 50 ml,\nlime - 2 slices,\nmint - 15 gr.,\nsimple syrup - 20 gr."},
+          2 => {name: 'Cuba libre', recipe: "Black rum - 50 ml\nCola - 150 ml\nFresh lime juice - 10 ml"},
+          3 => {name: 'Black russian', recipe: "Vodka - 50 ml\nKahlua - 50 ml"},
+          4 => {name: 'God father', recipe: "Scotch - 35 ml\nAmaretto - 35 ml"},
+      }
       if (1..4).include?(choice)
-        cocktail = ClassicCocktails.cc_set[choice]
+        cocktail = cocktails[choice]
         cocktail.each{|key, value| puts key, value}
       else
         begin
-        raise WrongNumberError.new
+          raise WrongNumberError.new
+        rescue WrongNumberError => e
+          p e.message
+          p e.backtrace
+        ensure
+          puts "Enjoy the cocktail and try another recipe from classic cocktails!"
+        end
+      end
+    end
+
+  end
+end
+
+class NewEraBar
+  include Welcome
+  include Guest
+
+  def run2
+
+    begin
+      puts "Hello i'm #{@name}, and i need some #{drinks}, what you can offer?"
+      puts "Here is a coctails, you can choose one and we show ingredients:"
+      puts "Bramble - 1\nFrench martini - 2\nSpritz veneziano - 3\nVesper - 4\nMake your choice: "
+      choice = gets.to_i
+      cocktails = {
+          1 => {name: 'Bramble', recipe: "Gin - 40 ml,\nSugar syrup - 10 ml,\nFresh lemon juice - 15 ml.,\nBlackberry liqueur - 15 ml."},
+          2 => {name: 'French martini', recipe: "Vodka - 45 ml\nRaspberry liqueur - 15 ml.\nFresh pineapple juice - 15 ml"},
+          3 => {name: 'Spritz veneziano', recipe: "Prosseco - 60 ml\nAperol - 40 ml\nSplash of Soda water."},
+          4 => {name: 'Vesper', recipe: "Gin - 60 ml\nVodka - 15 ml\nLillet Blonde - 7 ml."},
+      }
+      if (1..4).include?(choice)
+        cocktail = cocktails[choice]
+        cocktail.each{|key, value| puts key, value}
+      else
+        begin
+          raise WrongNumberError.new
         rescue WrongNumberError => e
           p e.message
           p e.backtrace
@@ -64,30 +93,14 @@ class InShakerClassic
   end
 end
 
-class InShakerNewEra
-  include Welcome
-  extend NewEra
-
-  def run2
-    begin
-      puts "Here is a coctails, you can choose one and we show ingredients:"
-      puts "Bramble - 1\nFrench martini - 2\nSpritz veneziano - 3\nVesper - 4\n"
-      puts "Make your choice: "
-      choice = gets.to_i
-      cocktail = NewEra.ne_set[choice]
-      cocktail.each{|key, value| puts key, value}
-    rescue NoMethodError => e
-      p e.message
-      p e.backtrace
-    retry
-    ensure
-      puts "Enjoy the cocktail and try another recipe from New era cocktails!"
-    end
-  end
-end
 
 
-InShakerClassic.new.greeting("classic")
-InShakerClassic.new.run1
-InShakerNewEra.new.greeting("new era")
-InShakerNewEra.new.run2
+
+
+guest1 = ClassicCocktailsBar.new("John", "classic cocktails")
+  guest1.greeting("Classic")
+  guest1.run1
+
+guest2 = NewEraBar.new("Bob", "modern cocktails")
+  guest2.greeting("NewEra")
+  guest2.run2
