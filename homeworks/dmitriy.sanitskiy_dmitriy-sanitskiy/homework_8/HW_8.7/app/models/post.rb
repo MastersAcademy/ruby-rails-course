4d4405087dd5d8f81f2606cd65fa3f7c576fe897
dmitriy.sanitskiy <dmitriy.sanitskiy@gmail.com>
2017-01-28 23:54:28 +0200

    Sinatra App

--- homeworks/dmitriy.sanitskiy_dmitriy-sanitskiy/homework_8/HW_8.7/app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ActiveRecord::Base
-  belongs_to :user
-  validates :title, presence: true
-  validates :body, presence: true
-end
--- homeworks/dmitriy.sanitskiy_dmitriy-sanitskiy/homework_8/HW_8.7/config/constants.rb
+++
@@ -1,3 +0,0 @@
-SITE_NAME = 'Myapp'
-SITE_URL = 'http://127.0.0.0:4567'
-SITE_SUBTITLE = 'Site'
